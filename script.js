afac078db952c6447837b4528ad7a5c4d0f32d8b
linfeng Chen <ku2003ku@gmail.com>
2022-06-06 12:24:11 -0400

    guessgame-v1

---
+++ script.js
@@ -0,0 +1,46 @@
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.message') =
+document.querySelector('.score').textContent = score;
+//
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️   No secretNumber';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉   Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      score = 0;
+      document.querySelector('.message').textContent = 'Sorry you lost';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      score = 0;
+      document.querySelector('.message').textContent = 'Sorry you lost';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
